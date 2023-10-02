fe8882ba6bef57ecd48645c46f01b60ad9031100
Kshitij Ingale <kshitijingale2@gmail.com>
2023-10-02 10:42:01 +0530

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./**/*.{html,js}"],
+  theme: {
+    extend: {},
+    colors: {
+      "header-bg-color": "#4e4e98",
+      "link-color": "#3090a5",
+      "link-hover-color": "rgb(24, 88, 102)"
+    }
+  },
+  plugins: [],
+}
+
